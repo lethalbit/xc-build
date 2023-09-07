171b73d63273a3ab7ef4fcdb59c63599f6aba6a1
Aki Van Ness <aki@lethalbit.net>
2023-09-07 02:47:08 -0700

    added a `get_paths` call to the components to generate a list of paths and details for component versions

--- xc_build/data/__init__.py
+++ xc_build/data/__init__.py
@@ -1,15 +1,18 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import json
-from importlib import resources
-from pathlib   import Path
+from importlib       import resources
+from typing          import Optional
+from pathlib         import Path
 
-from ..config  import (
+from ..core.strutils import expand_variables
+from ..config        import (
 	SOURCE_DIR, DLD_DIR
 )
 
 __all__ = (
 	'load_components',
+	'get_paths',
 	'component_archive_path',
 	'component_source_path'
 )
@@ -28,3 +31,40 @@ def load_components() -> dict:
 	return json.loads(
 		resources.read_text(__name__, 'components.json')
 	)['components']
+
+
+def get_paths(components: Optional[dict] = None, for_all: bool = False) -> list[dict]:
+	if components is None:
+		components = load_components()
+
+	component_paths = list()
+
+	for name, details in components.items():
+		if for_all:
+			for ver in details['versions']:
+				component_paths.append({
+					'component': name,
+					'url': expand_variables(
+						f'{details["url"]}/{details["filename"]}', {'VERSION': ver['version']}
+					),
+					'version': ver['version'],
+					'archive_path': component_archive_path(name, ver['version'], details['filename']),
+					'source_path': component_source_path(name, ver['version']),
+					'sha512sum': ver['sha512sum']
+				})
+		else:
+			component_paths.append({
+				'component': name,
+				'url': expand_variables(
+					f'{details["url"]}/{details["filename"]}', {'VERSION': details['latest']}
+				),
+				'version': details['latest'],
+				'archive_path': component_archive_path(name, details['latest'], details['filename']),
+				'source_path': component_source_path(name, details['latest']),
+				'sha512sum': list(filter(
+					lambda v: v['version'] == details['latest'],
+					details['versions']
+				))[0]['sha512sum']
+			})
+
+	return component_paths
